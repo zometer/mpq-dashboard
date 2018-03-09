935a980a801f5a69df625106347078b41d58f012
Christopher Spears <zometer@gmail.com>
2018-03-09 00:29:12 -0600

    initial import

---
+++ docker/mpq-db/init-mpq-db.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE USER $MPQ_DB_USER;
+    CREATE DATABASE mpq_data;
+    ALTER DATABASE mpq_data SET search_path TO mpq_data;
+    GRANT ALL PRIVILEGES ON DATABASE mpq_data TO $MPQ_DB_USER;
+EOSQL
+
+env
+
+FILE="$MPQ_DB_INIT_DIR/sql/schema/MPQ_DATA.sql" && echo "processing $FILE" && psql -v ON_ERROR_STOP=1 -d mpq_data --username "$MPQ_DB_USER" -f $FILE
+FILE="$MPQ_DB_INIT_DIR/sql/table/COLOR.sql" && echo "processing $FILE" && psql -v ON_ERROR_STOP=1 -d mpq_data --username "$MPQ_DB_USER" -f $FILE
+FILE="$MPQ_DB_INIT_DIR/sql/table/MPQ_CHARACTER.sql" && echo "processing $FILE" && psql -v ON_ERROR_STOP=1 -d mpq_data --username "$MPQ_DB_USER" -f $FILE
+FILE="$MPQ_DB_INIT_DIR/sql/table/CHARACTER_POWER.sql" && echo "processing $FILE" && psql -v ON_ERROR_STOP=1 -d mpq_data --username "$MPQ_DB_USER" -f $FILE
+
+echo "setting default schema" && psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "ALTER DATABASE mpq_data SET search_path TO mpq_data;" 
---
+++ docker/mpq-db/sql/schema/MPQ_DATA.sql
@@ -0,0 +1,1 @@
+create schema mpq_data ; 
