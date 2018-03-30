0290435138a608912b1f4b9a0ce19ff806eeab7a
Christopher Spears <zometer@gmail.com>
2018-03-29 23:41:35 -0500

    populating the color table at db startup

--- docker/mpq-db/init-mpq-db.sh
+++ docker/mpq-db/init-mpq-db.sh
@@ -10,9 +10,13 @@ EOSQL
 
 env
 
+# create db objects
 FILE="$MPQ_DB_INIT_DIR/sql/schema/MPQ_DATA.sql" && echo "processing $FILE" && psql -v ON_ERROR_STOP=1 -d mpq_data --username "$MPQ_DB_USER" -f $FILE
 FILE="$MPQ_DB_INIT_DIR/sql/table/COLOR.sql" && echo "processing $FILE" && psql -v ON_ERROR_STOP=1 -d mpq_data --username "$MPQ_DB_USER" -f $FILE
 FILE="$MPQ_DB_INIT_DIR/sql/table/MPQ_CHARACTER.sql" && echo "processing $FILE" && psql -v ON_ERROR_STOP=1 -d mpq_data --username "$MPQ_DB_USER" -f $FILE
 FILE="$MPQ_DB_INIT_DIR/sql/table/CHARACTER_POWER.sql" && echo "processing $FILE" && psql -v ON_ERROR_STOP=1 -d mpq_data --username "$MPQ_DB_USER" -f $FILE
 
-echo "setting default schema" && psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "ALTER DATABASE mpq_data SET search_path TO mpq_data;" 
+echo "setting default schema" && psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "ALTER DATABASE mpq_data SET search_path TO mpq_data;"
+
+# load init data
+FILE="$MPQ_DB_INIT_DIR/sql/load/load-color.sql" && echo "processing $FILE" && psql -v ON_ERROR_STOP=1 -d mpq_data --username "$POSTGRES_USER" -f $FILE
---
+++ docker/mpq-db/sql/load/load-color.sql
@@ -0,0 +1,4 @@
+copy mpq_data.color (name, display_name)
+from '/tmp/mpq-db/data/color.csv'
+with csv header
+;
